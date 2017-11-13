91a302eafba6bcfd184564e10942e3f3b31c1694
arifrahmans <arif.ilkom@gmail.com>
2017-11-13 20:33:50 +0700

    fix comment and post input

--- graphql/types/comment_input.js
+++ graphql/types/comment_input.js
@@ -8,7 +8,7 @@ import {
 export default new GraphQLInputObjectType({
     name : 'CommentInput',
     fields : {
-        _id : {type : new GraphQLNonNull(GraphQLID)},
+        _id : {type : GraphQLID},
         postId : {type : new GraphQLNonNull(GraphQLID)},
         userId : {type : new GraphQLNonNull(GraphQLID)},
         message : {type : new GraphQLNonNull(GraphQLString)},
--- graphql/types/post_input.js
+++ graphql/types/post_input.js
@@ -9,6 +9,7 @@ export default new GraphQLInputObjectType({
     name : 'PostInput',
     fields : {
         _id : {type : GraphQLID},
+        userId : {type : new GraphQLNonNull(GraphQLID)},
         title : {type : new GraphQLNonNull(GraphQLString)},
         description : {type : new GraphQLNonNull(GraphQLString)},
     }
